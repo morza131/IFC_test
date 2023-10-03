e5171520545456f2d4bbd0fc09c0ecbda9728953
morza131 <morza131@gmail.com>
2023-10-03 21:33:49 +0300

    Добавьте файлы проекта.

---
+++ IFC_test/IgnorePropertiesResolver.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFC_test
+{
+    internal class IgnorePropertiesResolver : DefaultContractResolver
+    {
+        private readonly HashSet<string> _propertiesToIgnore;
+
+        public IgnorePropertiesResolver(params string[] propertiesToIgnore)
+        {
+            _propertiesToIgnore = new HashSet<string>(propertiesToIgnore, StringComparer.OrdinalIgnoreCase);
+        }
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+
+            if (_propertiesToIgnore.Contains(property.PropertyName))
+            {
+                property.ShouldSerialize = instance => false;
+            }
+
+            return property;
+        }
+    }
+}
---
+++ IFC_test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Xbim.Common;
+using Xbim.Ifc;
+using Xbim.Ifc2x3.ProductExtension;
+
+namespace IFC_test
+{
+    internal class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            //Открытие исходного файла
+            string openFilePath = String.Empty;
+            string saveFilePath = String.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "IFC files (*.ifc)|*.ifc";
+                openFileDialog.Title = "Выберите исходный файл тестового задания";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    openFilePath = openFileDialog.FileName;
+                    Stream fileStream = openFileDialog.OpenFile();
+                }
+            }
+            using (IfcStore model = IfcStore.Open(openFilePath))
+            {                
+                using (ITransaction txn = model.BeginTransaction("Transaction"))
+                {                    
+                    List<IfcBuildingElementProxy> objects = model.Instances
+                                                                 .OfType<IfcBuildingElementProxy>()
+                                                                 .ToList(); //Все объекты категории "Обобщённые модели"
+                    IfcBuildingElementProxy elDrive = model.Instances
+                                                           .OfType<IfcBuildingElementProxy>()
+                                                           .Where<IfcBuildingElementProxy>(x => x.FriendlyName
+                                                           .Contains("ЭП_КП"))
+                                                           .FirstOrDefault(); //Нужный объект
+                    foreach (IfcBuildingElementProxy element in objects) //Удаление всех объектов "Обобщённые модели", кроме нужного
+                    {
+                        if (element != elDrive)
+                            model.Delete(element);
+                    }                    
+                    txn.Commit();
+                };
+                // Сохранение результатов
+                Stream myStream;
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "IFC files (*.ifc)|*.ifc";
+                saveFileDialog.Title = "Выберите путь сохранения файла";                
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if ((myStream = saveFileDialog.OpenFile()) != null)
+                    {
+                        saveFilePath = saveFileDialog.FileName;                        
+                        myStream.Close();
+                    }
+                }
+                model.SaveAs(saveFilePath);
+            }           
+        }
+    }
+}
+
+
+
+
+
+
+
+
